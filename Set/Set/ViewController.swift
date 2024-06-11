a46e80e4ae0512e584bbc384fe9236b7bcbecea9
Elena Khaletska <yeliena.khaletska@pipedrive.com>
2024-06-11 16:54:01 +0300

    Add possibility to choose cards, draw more cards and start new game

--- Set/Set/ViewController.swift
+++ Set/Set/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    private var game: SetGame!
+    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet weak var drawThreeMoreCardsButton: UIButton!
+    @IBOutlet weak var newGameButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        startNewGame()
     }
 
+    @IBAction private func drawThreeMoreCardsButtonTapped() {
+        self.game.drawCards(3)
+        updateUI()
+    }
+
+    @IBAction private func newGameButtonTapped() {
+        startNewGame()
+    }
+
+    @IBAction private func cardButtonTapped(_ button: UIButton) {
+        if let index = self.cardButtons.firstIndex(of: button) {
+            self.game.touchCard(index: index)
+            updateUI()
+        }
+    }
+
+    private func startNewGame() {
+        self.game = SetGame()
+        self.game.drawCards(12)
+        updateUI()
+    }
+
+    private func updateUI() {
+        self.drawThreeMoreCardsButton.isEnabled = self.game.canDrawCards
+
+        for (index, button) in self.cardButtons.enumerated() {
+            if let card = self.game.getCard(for: index) {
+                button.configuration?.background.strokeColor = self.game.isCardChosen(at: index) ? .orange : .clear
+                button.isHidden = false
+                button.setTitle(card.content, for: .normal)
+            }
+            else {
+                button.isHidden = true
+            }
+        }
+    }
 
 }
 
